cde201e49c9ac76590141ab2a4fe30007911e431
Teemu Ruokokoski <truoko@gmail.com>
2023-04-19 13:55:07 +0300

    Message fix

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: error.message })
   }
   next(error)
+  return response.status(500).json({ error: error.message }) // korjaus virheiden näyttämiseen
 }
 
 app.use(errorHandler)
